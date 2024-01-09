60cf9117e890aafa8b352ce84bf2eb95037848b5
unknown <alooshkaled7@gmail.com>
2024-01-09 09:05:13 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    // if( localStorage.getItem("permission") == 1){
+    //     if (localStorage.getItem("permission") == 1) {
+    //         document.getElementsByClassName('hideUsers')[0].style.display = "none";
+    //         document.getElementsByClassName('hideUsers')[1].style.display = "none";
+    //         document.getElementsByClassName('hideUsers')[2].style.display = "none";
+    //         document.getElementsByClassName('hideUsers')[3].style.display = "none";
+    //         document.getElementsByClassName('hideUsers')[4].style.display = "none";
+
+    //       }    }
+
+    $("img").click(function(){this.requestFullscreen()})
+});
+
+
